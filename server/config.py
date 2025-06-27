07f4b803c161e685999181952cc84d58941a0665
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-06-27 21:16:52 +0300

    more deployment updates

--- server/config.py
+++ server/config.py
@@ -5,16 +5,9 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-
-from flask import Flask
-from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://artist:6MIu7fr8KF3p8ZADa8CJQwpQflmwNprd@dpg-d1fao3ali9vc739r081g-a.oregon-postgres.render.com/artistdb_f7o7'
+DATABASE_URI = 'postgresql://artist:6MIu7fr8KF3p8ZADa8CJQwpQflmwNprd@dpg-d1fao3ali9vc739r081g-a.oregon-postgres.render.com/artistdb_f7o7'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
